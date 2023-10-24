3724fd503083b18d69f5f9e22cbe5182788ad750
Antoyrie Green <antoyrieg@gmail.com>
2023-10-24 09:16:09 -0400

    Add files via upload

---
+++ Pages/experience.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portfolio.website.Pages
+{
+    public class experienceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
